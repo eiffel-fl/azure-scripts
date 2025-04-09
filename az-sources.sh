eiffel-fl/azure-scripts

commit f9572fce78fc12c2d227a8cd712583083952a0b8

az-sources: Add vnet IP to VM when using bastion.
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

az-sources.sh
@@ -114,10 +114,13 @@ function create_vm {
 
 	subnet_args=''
 	if [ "${use_bastion}" = 'false' ]; then
-		subnet_args="--subnet $(get_kv1_id)"
+		net_args="--subnet $(get_kv1_id)"
+	else
+		# As created in create_vnet().
+		net_args='--vnet-address-prefix 10.10.0.0/16'
 	fi
 
-	az vm create --resource-group $resource_group --name $vm $subnet_args --image $image --admin-username ${resource_prefix} --generate-ssh-keys --size $vm_size --os-disk-size-gb $disk_size --security-type Standard -o none
+	az vm create --resource-group $resource_group --name $vm $net_args --image $image --admin-username ${resource_prefix} --generate-ssh-keys --size $vm_size --os-disk-size-gb $disk_size --security-type Standard -o none
 
 	# To extend OS disk space of an already existing VM, you can do the following:
 # 	disk_name=$(az disk list --resource-group $resource_group --query '[*].{Name:name,Gb:diskSizeGb,Tier:accountType}' -o tsv | grep $vm | cut -f1)

az-vm.sh
@@ -104,7 +104,7 @@ if [ "${bastion}" = 'true' ]; then
 	cat << EOF
 VM was created.
 You can now connect to it using:
-* Either: az network bastion ssh --name $bastion --resource-group $resource_group --target-ip-addres $vm_ip --auth-type "ssh-key" --username $vm_username --ssh-key ~/.ssh/id_rsa
+* Either: az network bastion ssh --name $bastion --resource-group $resource_group --target-ip-address $vm_ip --auth-type "ssh-key" --username $vm_username --ssh-key ~/.ssh/id_rsa
 * Or: sudo az network bastion tunnel --name $bastion --resource-group $resource_group --target-ip-address $vm_ip --resource-port 22 --port 1337; ssh $vm_username@127.0.0.1 -p 1337
 To use scp or sftp, the tunnel is mandatory.
 EOF

commit 9f75427784eb63ba9ff741e33c457088cc4a3579

az-sources: Use default subnet to create VM when using Bastion.
Fixes: f9572fce78fc ("")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

az-sources.sh
@@ -94,11 +94,12 @@ function create_vm {
 	local disk_size
 	local image
 	local use_bastion
+	local vnet_name
 
 	local vm
 
-	if [ $# -lt 6 ]; then
-		echo "${FUNCNAME[0]} needs 5 arguments: the resource_prefix, the resource_group, the vm_size, disk_size, the image and use_bastion" 1>&2
+	if [ $# -lt 7 ]; then
+		echo "${FUNCNAME[0]} needs 7 arguments: the resource_prefix, the resource_group, the vm_size, disk_size, the image, use_bastion and vnet_name" 1>&2
 
 		exit 1
 	fi
@@ -109,6 +110,7 @@ function create_vm {
 	disk_size=$4
 	image=$5
 	use_bastion=$6
+	vnet_name=$7
 
 	vm="${resource_prefix}vm"
 
@@ -116,11 +118,12 @@ function create_vm {
 	if [ "${use_bastion}" = 'false' ]; then
 		net_args="--subnet $(get_kv1_id)"
 	else
-		# As created in create_vnet().
-		net_args='--vnet-address-prefix 10.10.0.0/16'
+		# We have to use default as subnet instead of Azure Bastion, otherwise
+		# an error is thrown.
+		net_args="--vnet-name ${vnet_name} --subnet default"
 	fi
 
-	az vm create --resource-group $resource_group --name $vm $net_args --image $image --admin-username ${resource_prefix} --generate-ssh-keys --size $vm_size --os-disk-size-gb $disk_size --security-type Standard -o none
+	az vm create --resource-group $resource_group --name $vm $net_args --image $image --admin-username $resource_prefix --generate-ssh-keys --size $vm_size --os-disk-size-gb $disk_size --security-type Standard -o none
 
 	# To extend OS disk space of an already existing VM, you can do the following:
 # 	disk_name=$(az disk list --resource-group $resource_group --query '[*].{Name:name,Gb:diskSizeGb,Tier:accountType}' -o tsv | grep $vm | cut -f1)

az-vm.sh
@@ -18,7 +18,7 @@ resource_prefix=$(whoami)
 SIZE_FORMAT='Standard_D%d%cs_v5'
 location='westeurope'
 architecture='a'
-bastion='false'
+use_bastion='false'
 core_count=64
 disk_size=128
 os='Ubuntu'
@@ -29,7 +29,7 @@ while getopts "abc:l:o:n:h" option; do
 		architecture='p'
 		;;
 	b)
-		bastion='true'
+		use_bastion='true'
 		;;
 	c)
 		core_count=${OPTARG}
@@ -47,7 +47,7 @@ while getopts "abc:l:o:n:h" option; do
 		echo "Usage: $0 [-n resource_prefix] [-a] [-b] [-c core_count] [-l location] [-o os_sku]" 1>&2
 		echo -e "\t-n: The given string will be used as resource prefix, $(whoami) by default." 1>&2
 		echo -e "\t-a: Use Ampere Altra (i.e. arm64) node, AMD by default." 1>&2
-		echo -e "\t-b: Use bastion to ssh to VM, does not use bastion by default." 1>&2
+		echo -e "\t-b: Use bastion to ssh to VM, does not use use_bastion by default." 1>&2
 		echo -e "\t-c: The given number will be used as node size, 64 cores by default." 1>&2
 		echo -e "\t-l: The given string will be used as location, westeurope by default." 1>&2
 		echo -e "\t-o: The given string will be used as os-sku, Ubuntu by default." 1>&2
@@ -81,7 +81,7 @@ esac
 
 az login --scope https://management.core.windows.net//.default
 
-if [ "${bastion}" = 'false' ]; then
+if [ "${use_bastion}" = 'false' ]; then
 	# kv1 is only available in a given subscription.
 	current_subscription=$(az account show -o tsv --query name)
 	az account set -s '47635d02-50bb-4f1f-8b44-e9e9518015e6'
@@ -92,11 +92,11 @@ resource_group=$(create_resource_group $resource_prefix $location)
 # Craft the size string
 vm_size=$(printf $SIZE_FORMAT $core_count $architecture)
 
-if [ "${bastion}" = 'true' ]; then
+if [ "${use_bastion}" = 'true' ]; then
 	vn=$(create_vnet $resource_prefix $resource_group)
 	# Creating a bastion takes aaaaaaages!
 	bastion=$(create_bastion $resource_prefix $resource_group $vn $location)
-	vm=$(create_vm $resource_prefix $resource_group $vm_size $disk_size $image $bastion)
+	vm=$(create_vm $resource_prefix $resource_group $vm_size $disk_size $image $use_bastion $vn)
 
 	vm_ip=$(get_vm_private_ip $resource_group $vm)
 	vm_username=$(get_vm_username $resource_group $vm)
@@ -105,7 +105,7 @@ if [ "${bastion}" = 'true' ]; then
 VM was created.
 You can now connect to it using:
 * Either: az network bastion ssh --name $bastion --resource-group $resource_group --target-ip-address $vm_ip --auth-type "ssh-key" --username $vm_username --ssh-key ~/.ssh/id_rsa
-* Or: sudo az network bastion tunnel --name $bastion --resource-group $resource_group --target-ip-address $vm_ip --resource-port 22 --port 1337; ssh $vm_username@127.0.0.1 -p 1337
+* Or: sudo az network use_bastion tunnel --name $bastion --resource-group $resource_group --target-ip-address $vm_ip --resource-port 22 --port 1337; ssh $vm_username@127.0.0.1 -p 1337
 To use scp or sftp, the tunnel is mandatory.
 EOF
 else
@@ -116,7 +116,7 @@ else
 	fi
 
 	# Otherwise, create a VM using kv1.
-	vm=$(create_vm $resource_prefix $resource_group $vm_size $disk_size $image $bastion)
+	vm=$(create_vm $resource_prefix $resource_group $vm_size $disk_size $image $use_bastion)
 
 	echo -e "VM was created.\nYou should be able to connect using: ssh $(get_vm_username $resource_group $vm)@$(get_vm_private_ip $resource_group $vm)"
 